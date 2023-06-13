072b158176c22d47e6d6f2d4212f25152ae2ce75
kumaragurumkce <kumaragururf123@gmail.com>
2023-06-13 22:22:11 +0530

    first commit

--- Rock/rock.js
+++ Rock/rock.js
@@ -2,14 +2,21 @@ let inputids=document.getElementById("inputid");
 let submits=document.getElementById("submit");
 let shopCollecs=document.getElementById("shopCollec");
 submits.addEventListener("click",()=>{
-      const li=document.createElement("li");
-      const text=document.createTextNode(inputids.value);
-      li.className="shopitem";
-      li.append(text);
-      shopCollecs.append(li);
-    })
+         let newitem=document.createElement("li");
+         let divs=document.createElement("div");
+         let divbtn=document.createElement("div");   
+         newitem.append(divs,divbtn);           
+     divs.textContent=inputids.value;
+        newitem.className="shopitem";
+        divbtn.parentElement.setAttribute("onClick","removeItem(event)");
+        divbtn.innerHTML=`<i>X</i>`;
+        newitem.append(divbtn);
+        shopCollecs.append(newitem);
+        inputids.value = "";
+})
 function removeItem(event){ 
 let existItem=event.target.parentNode.parentNode;
 console.log(event.target.parentNode.parentNode);
 existItem.remove();
 }
+
