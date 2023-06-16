d3a4d241c0059b7cacfab1b23eab123ef7a3e892
kumaragurumkce <kumaragururf123@gmail.com>
2023-06-16 14:51:39 +0530

    Update

--- Rock/rock.js
+++ Rock/rock.js
@@ -1,22 +1,22 @@
 let inputids=document.getElementById("inputid");
 let submits=document.getElementById("submit");
 let shopCollecs=document.getElementById("shopCollec");
-submits.addEventListener("click",()=>{
-         let newitem=document.createElement("li");
-         let divs=document.createElement("div");
-         let divbtn=document.createElement("div");   
-         newitem.append(divs,divbtn);           
-     divs.textContent=inputids.value;
-        newitem.className="shopitem";
-        divbtn.parentElement.setAttribute("onClick","removeItem(event)");
-        divbtn.innerHTML=`<i>X</i>`;
-        newitem.append(divbtn);
-        shopCollecs.append(newitem);
-        inputids.value = "";
+submits.addEventListener("click",(e)=>{
+             e.preventDefault();
+            const newlist=document.createElement("li");
+           const div=document.createElement("div");
+           const divremo=document.createElement("div");
+           newlist.append(div,divremo);
+           div.textContent=inputids.value;   
+           newlist.className="shopitem";
+           divremo.parentElement.setAttribute("onClick","removeItem(event)" );
+           divremo.innerHTML=`<i>X</i>`;            
+         newlist.append(divremo);
+         shopCollecs.append(newlist); 
+         //div.append(divremo);
+               inputids.value="";
 })
 function removeItem(event){ 
 let existItem=event.target.parentNode.parentNode;
-console.log(event.target.parentNode.parentNode);
 existItem.remove();
 }
-
